6a3e6bae7e555c11ba3e5aeca522d6393ffc5575
vaishali <vaishalimehta092@gmail.com>
2023-06-21 01:39:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'image-upload';
+  url:String= '';
+  sendData = {name: '', size: '', type: ''};
+  constructor(private httpClient: HttpClient) {}
+
+  onSelectFile(event:any) {
+    if (event.target.files) {
+      console.log(event.target.files);
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (e:any) => {
+        this.url = e.target.result;
+      }
+    }
+    this.sendData.name = event.target.files[0].name;
+    this.sendData.size = event.target.files[0].size;
+    this.sendData.type = event.target.files[0].type;
+  
+  }
+
+  upload() {
+    this.httpClient.post('http://localhost:3000/posts', this.sendData).subscribe((res: any) => {
+      console.log(res);
+    });
+  }
 }
